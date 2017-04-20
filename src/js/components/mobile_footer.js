2f92ca493f9ea9a5f8b97218af2e995b85d91e27
pine nut <627605616@qq.com>
2017-04-20 15:24:10 +0800

    react-news-page

--- src/js/components/mobile_footer.js
+++ src/js/components/mobile_footer.js
@@ -1,28 +1,18 @@
 import React from 'react';
 import {Row, Col} from 'antd';
-import {Menu, Icon} from 'antd';
-const SubMenu = Menu.SubMenu;
-const MenuItemGroup = Menu.ItemGroup;
-
 export default class MobileFooter extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            current: 'top'
-        };
-    }
-    render() {
-        return (
-            <footer>
-                <Row>
-                    <Col span={2}></Col>
-                    <Col span={20} class="footer">
-                      fffffffffffffffffffffffffffffffffffff
-                    </Col>
 
-                    <Col span={2}></Col>
-                </Row>
-            </footer>
-        );
-    }
+	render() {
+		return (
+			<footer>
+				<Row>
+					<Col span={2}></Col>
+					<Col span={20} class="footer">
+            &copy;&nbsp;2016 ReactNews. All Rights Reserved.
+					</Col>
+					<Col span={2}></Col>
+				</Row>
+			</footer>
+		);
+	};
 }
