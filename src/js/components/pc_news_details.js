baf3067cb4275b984bb2b9732f76ab1986c97644
pine nut <627605616@qq.com>
2017-04-24 09:36:35 +0800

    思维导图

--- src/js/components/pc_news_details.js
+++ src/js/components/pc_news_details.js
@@ -5,43 +5,43 @@ import PCFooter from './pc_footer';
 import PCNewsImageBlock from './pc_news_image_block';
 import CommonComments from './common_comments';
 export default class PCNewsDetails extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-			newsItem: ''
-		};
-	};
-	componentDidMount() {
-		var myFetchOptions = {
-			method: 'GET'
-		};
-		fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=getnewsitem&uniquekey=" + this.props.params.uniquekey, myFetchOptions).then(response => response.json()).then(json => {
-			this.setState({newsItem: json});
-			document.title = this.state.newsItem.title + " - React News | React 驱动的新闻平台";
-		});
-	};
-	createMarkup() {
-		return {__html: this.state.newsItem.pagecontent};
-	};
-	render() {
-		return (
-			<div>
-				<PCHeader></PCHeader>
-				<Row>
-					<Col span={2}></Col>
-					<Col span={14} className="container">
-						<div class="articleContainer" dangerouslySetInnerHTML={this.createMarkup()}></div>
-						<hr/>
-						<CommonComments uniquekey={this.props.params.uniquekey}/>
-					</Col>
-					<Col span={6}>
-						<PCNewsImageBlock count={40} type="top" width="100%" cardTitle="相关新闻" imageWidth="150px"/>
-					</Col>
-					<Col span={2}></Col>
-				</Row>
-				<PCFooter></PCFooter>
-				<BackTop/>
-			</div>
-		);
-	};
+    constructor() {
+        super();
+        this.state = {
+            newsItem: ''
+        };
+    };
+    componentDidMount() {
+        var myFetchOptions = {
+            method: 'GET'
+        };
+        fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=getnewsitem&uniquekey=" + this.props.params.uniquekey, myFetchOptions).then(response => response.json()).then(json => {
+            this.setState({newsItem: json});
+            document.title = this.state.newsItem.title + " - React News | React 驱动的新闻平台";
+        });
+    };
+    createMarkup() {
+        return {__html: this.state.newsItem.pagecontent};
+    };
+    render() {
+        return (
+            <div>
+                <PCHeader></PCHeader>
+                <Row>
+                    <Col span={2}></Col>
+                    <Col span={14} className="container">
+                        <div class="articleContainer" dangerouslySetInnerHTML={this.createMarkup()}></div>
+                        <hr/>
+                        <CommonComments uniquekey={this.props.params.uniquekey}/>
+                    </Col>
+                    <Col span={6}>
+                        <PCNewsImageBlock count={40} type="top" width="100%" cardTitle="相关新闻" imageWidth="150px"/>
+                    </Col>
+                    <Col span={2}></Col>
+                </Row>
+                <PCFooter></PCFooter>
+                <BackTop/>
+            </div>
+        );
+    };
 }
